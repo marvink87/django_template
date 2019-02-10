5c00bc6576864ccbde4b16e79e0463c32448e8f2
Marvin Kuurstra <marvin@kuurstra.com>
2019-02-10 18:31:07 +0100

    Change to template

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,4 @@
-"""django_auth URL Configuration
+"""{{ project_name }} URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
