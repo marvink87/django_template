31b44f1dc5f31c80f5ebf0615430678a35ba79bb
Marvin Kuurstra <marvin@kuurstra.com>
2019-03-01 15:38:46 +0100

    Make login required for profile edit

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -12,7 +13,7 @@ class SignUp(generic.CreateView):
     template_name = 'users/signup.html'
 
 
-class UpdateProfile(generic.UpdateView):
+class UpdateProfile(LoginRequiredMixin, generic.UpdateView):
     form_class = CustomUserChangeForm
     success_url = reverse_lazy('profile')
     template_name = 'users/profileedit.html'
